2498517817cf84dcf03171e1d2a91285788a3dcc
pixelbyaj <abhishek2185@gmail.com>
2023-07-07 20:09:55 +0200

    Updates

---
+++ precache-manifest.f65472d1cf6efcbe42453ce203e797cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b5ce52c1b2539f73755747b6d0e447",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d540b10ba07781f43433",
+    "url": "./static/css/2.d87de7ff.chunk.css"
+  },
+  {
+    "revision": "5e7fb5466900afcfa0ad",
+    "url": "./static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "d540b10ba07781f43433",
+    "url": "./static/js/2.51e6b2e5.chunk.js"
+  },
+  {
+    "revision": "e040ed6f8844fc18aee84baa8cddc65f",
+    "url": "./static/js/2.51e6b2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7fb5466900afcfa0ad",
+    "url": "./static/js/main.ea1897a6.chunk.js"
+  },
+  {
+    "revision": "d87b0bc6766ca0b85232",
+    "url": "./static/js/runtime-main.242c20df.js"
+  }
+]);
